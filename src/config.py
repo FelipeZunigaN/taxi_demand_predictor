fd3d9c7e1742634aeade2d1c1b44fe5acb78ecdb
Felipe Zuñiga <ing.fzuniga@gmail.com>
2024-09-07 23:24:31 -0400

    Updated notebooks + config

--- src/config.py
+++ src/config.py
@@ -8,9 +8,9 @@ load_dotenv(PARENT_DIR / '.env')
 
 HOPSWORKS_PROJECT_NAME = 'forecast_model'
 try:
-    HOPSWORKS_API_KEY  = os.environ['HOPSWORK_API_KEY']
+    HOPSWORKS_API_KEY  = os.environ['HOPSWORKs_API_KEY']
 except:
-    raise Exception("Create an .env file on the project root with the HOPSWORK_API_KEY")
+    raise Exception("Create an .env file on the project root with the HOPSWORKs_API_KEY")
 
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 1
